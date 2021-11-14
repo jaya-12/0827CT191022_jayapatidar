da965f1f7f987c8c1483e9cc03dfd19a4dcc9e31
jaya-12 <81163971+jaya-12@users.noreply.github.com>
2021-11-14 22:07:50 +0530

    Create 2) 7.py

---
+++ 2) 7.py
@@ -0,0 +1,17 @@
+balance_str = "1500"
+balance_int = int(balance_str)
+
+# print the type
+print(type(balance_int))
+
+# print the value
+print(balance_int)
+
+balance_str = "1500.4"
+balance_float = float(balance_str)
+
+# print the type
+print(type(balance_float))
+
+# print the value
+print(balance_float)
